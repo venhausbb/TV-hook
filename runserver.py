ac549546afc527c91c0ebd06d45c5e42a7e6c3ed
venhausbb <venhausbb@gmail.com>
2024-09-19 12:09:02 -0500

    Update runserver.py with port binding

--- runserver.py
+++ runserver.py
@@ -1,12 +1,9 @@
-import os
 from flask import Flask
-
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return "Flask app running!"
+def index():
+    return 'Hello, World!'
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Default to port 5000 if PORT is not set
-    app.run(host='0.0.0.0', port=port)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
